506934e2ccd369f157476cb98ab33eebbcf334ea
Erwann Messoah <tiwannbeats@gmail.com>
2025-02-04 13:59:27 +0100

    Grid

---
+++ Assets/Scripts/GridPlacement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GridPlacement : MonoBehaviour
+{
+
+    private static Vector3 Floor(Vector3 Vec)
+    {
+        return new Vector3(Mathf.Floor(Vec.x), Mathf.Floor(Vec.y), Mathf.Floor(Vec.z));
+    }
+    
+    private void Update()
+    {
+        Vector3 Position = transform.position;
+        transform.position = Floor(Position);
+    }
+}
