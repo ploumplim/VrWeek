2fc0f786c85ab43e2a4015df8c71766f2ba181ae
ploumplim <moreaubapt@gmail.com>
2025-02-10 01:29:57 +0100

    Push FInal , j'en peux plus

--- Assets/ObjectSnapping.cs
+++ Assets/ObjectSnapping.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
@@ -14,13 +15,16 @@ public class ObjectSnapping : MonoBehaviour
     public InputActionProperty snapButton; // Bouton pour snapper l'objet
     public LayerMask wallLayer;            // Couches détectées (mettre "Wall")
     public Transform controllerTransform;  // Transform du contrôleur VR
+    public GameObject bigObjectPrefab;     // Prefab de la version big de l'objet
+    public List<GameObject> objectsToTrack; // Liste des objets à suivre
+    public Boolean isBigObject;
 
     private XRGrabInteractable grabInteractable;
     private XRGeneralGrabTransformer grabTransformer;
 
     private Rigidbody rb;
     private Collider col;
-    private Quaternion savedRotation;      // Variable to save the rotation
+    private Dictionary<GameObject, Quaternion> initialRotations; // Dictionnaire pour sauvegarder les rotations initiales
 
     void Start()
     {
@@ -45,33 +49,58 @@ public class ObjectSnapping : MonoBehaviour
         {
             GetComponent<MeshCollider>().convex = true;
         }
+
+        // Initialiser le dictionnaire et sauvegarder les rotations initiales
+        initialRotations = new Dictionary<GameObject, Quaternion>();
+        foreach (var obj in objectsToTrack)
+        {
+            initialRotations[obj] = obj.transform.rotation;
+        }
     }
 
     private void OnGrab(SelectEnterEventArgs args)
     {
-        rb.isKinematic = false;
-        rb.useGravity = true;
+        Debug.Log("OnGrab called");
+
+        if (isBigObject)
+        {
+            Debug.Log("isBigObject is true in OnGrab");
+            gameObject.transform.localScale = new Vector3(10, 10, 10);
+        }
         rb.constraints = RigidbodyConstraints.FreezeRotation; // Verrouille la rotation
-        savedRotation = transform.rotation; // Save the current rotation
         transform.rotation = Quaternion.Euler(-90f, 0f, 0f); // Définit la rotation à -90°
     }
 
     private void OnRelease(SelectExitEventArgs args)
     {
+        Debug.Log("OnRelease called");
+
         Ray ray = new Ray(controllerTransform.position, controllerTransform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f, wallLayer))
+        Debug.DrawRay(controllerTransform.position, controllerTransform.forward * 100f, Color.red, 2f); // Visualiser le raycast dans la scène
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
         {
-            transform.position = RoundVar(hit.point);
-            transform.rotation = savedRotation; // Apply the saved rotation
+            Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+
+            bigObjectPrefab.transform.position = RoundVar(hit.point);
+            bigObjectPrefab.transform.rotation = Quaternion.Euler(-90f, 0f, 0f); // Set the rotation to -90, 0, 0
+
             rb.isKinematic = true;
             rb.useGravity = false;
             rb.constraints = RigidbodyConstraints.None; // Déverrouille la rotation
+
+            if (!isBigObject)
+            {
+                rb.gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.transform.localScale = new Vector3(100, 100, 100);
+            }
         }
         else
         {
-            rb.isKinematic = false;
-            rb.useGravity = true;
-            rb.constraints = RigidbodyConstraints.None; // Déverrouille la rotation
+            Debug.Log("Raycast did not hit any object in wallLayer");
         }
     }
 
